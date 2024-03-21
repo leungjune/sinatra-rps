9655f5335ac31c961300fe904b5196871c74f3cd
June Leung <148405089+leungjune@users.noreply.github.com>
2024-03-21 22:09:35 +0000

    initial commit - changes all files

--- app.rb
+++ app.rb
@@ -1,8 +1,52 @@
-require 'sinatra'
+# app.rb
 
-get('/') do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+require "sinatra"
+require "sinatra/reloader"
+
+moves = ["rock", "paper", "scissors"]
+
+get("/") do
+  erb(:rules)
+end
+
+get("/rock") do
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:rock, { :layout => :wrapper })
+end
+
+get("/paper") do
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:paper, { :layout => :wrapper })
+end
+
+get("/scissors") do
+  @comp_move = moves.sample
+  
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+  
+  erb(:scissors, { :layout => :wrapper })
 end
